e65d96ef770625ff2cb6bfb6260b06c517ac6ffc
Azucena Marroquin <atrevizo4@cnm.edu>
2023-10-20 09:30:04 -0600

    made some color updates and more mobile responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain':'contain',
+      '50%': '50%'
+    },
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"),require("daisyui")],
   daisyui: {
-    themes: ["autumn", "retro"],
+    themes: ["autumn", "bumblebee"],
   },
 }
 
