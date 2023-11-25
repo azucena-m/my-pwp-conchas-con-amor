951017d08c97da189d6b7e6baa391dc31b30bb12
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-25 09:31:15 -0700

    made changes to pwp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}",
+  "./index.html"],
   theme: {
     backgroundSize: {
       'auto': 'auto',
